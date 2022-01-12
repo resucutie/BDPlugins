8c9e2d9b9437d5fb551e0bc64904e62d9b377e28
abUwUser <pedro.santos.cartaxo@gmail.com>
2022-01-12 17:27:40 -0300

    fuck

--- plugins/Timezones/patches/contextMenu.tsx
+++ plugins/Timezones/patches/contextMenu.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Patcher, WebpackModules } from "@zlibrary"
+import { Patcher, Utilities, WebpackModules } from "@zlibrary"
 import { MenuGroup, MenuItem, MenuSeparator } from "@discord/contextmenu"
 import { Users } from "@discord/stores"
 
@@ -11,99 +11,195 @@ import { getTimezone } from "../utils/userList"
 import ThisPlugin from "../index"
 
 export default function(){
-    Patcher.after(WebpackModules.find(m => m.default?.displayName === 'GuildChannelUserContextMenu'), "default", (_this, [props], res) => {
-        const user: UserObject = props.user
+    /**
+     * Discord just made a lot of context menus lazyloaded. Sadly this is necessary
+     * Code made by Strencher, and adapted to work with BD by me. Credit strencher by his work
+     * @see {@link https://github.com/HolyMod/HolyMod/blob/master/src/renderer/autopatchers/contextmenu.tsx}
+     */
+    Patcher.before(WebpackModules.getByProps("openContextMenuLazy"), "openContextMenuLazy", (_this, args) => {
+        args[1] = (ogFunc => args => ogFunc(args).then(render => props => {
+            const firstRes = render(props)
 
-        if (user.bot || Users.getCurrentUser().id === user.id) return
+            try {
+                const ogFunc = firstRes?.type
 
-        let menuGroup = res.props?.children?.props?.children?.[4]
-        if (!menuGroup) return
+                if (!ogFunc) return firstRes
+                firstRes.type = (props) => {
+                    //here would be the equivalent of the callback in a normal patch
+                    useForceUpdate(); // Allow force re-rendering the entire context menu.
+                    const res = ogFunc.call(this, props)
 
-        const state = useAsync(async () => ({
-            offlineTz: await getTimezone(user.id, { includeTT: false }),
-            timezone: await getTimezone(user.id, { includeTT: settings.get("tt", true) })
-        }))
+                    switch (ogFunc?.displayName) {
+                        case "GuildChannelUserContextMenu": {
+                            patchGuildChannelUserContextMenu(this, props, res)
+                            break;
+                        }
 
-        menuGroup.props.children.unshift(<MenuGroup label={state.loading ? <div style={{ opacity: .3 }}>Loading...</div> : null}>
-            {!state.loading ? <>
-                {!state.value?.timezone ? <MenuItem
-                    id="timezones-add-user"
-                    label="Add timezone locally"
-                    action={async () => { await ThisPlugin.prototype.openSettingsModal(user.id) }}
-                /> : <MenuItem
-                    id="timezones-actions"
-                    label="Timezone actions"
-                >
-                    {TimestampActions({
-                        id: user.id,
-                        timezone: state.value?.timezone,
-                        onEditTimezone: async id => await ThisPlugin.prototype.openSettingsModal(id, Boolean(!state.value?.offlineTz && state.value?.timezone)),
-                        onDeleteTimezone: async id => {
-                            ThisPlugin.prototype.onDeleteTimezone(id)
-                        },
-                        isOnline: !state.value?.offlineTz
-                    })}
-                </MenuItem>}
-            </> : <>
-                <MenuItem
-                    id="timezones-loading-add"
-                    label="Add timezone locally"
-                    disabled={true}
-                />
-                <MenuItem
-                    id="timezones-loading-actions"
-                    label="Timezone actions"
-                    disabled={true}
-                />
-            </>}
-        </MenuGroup>, <MenuSeparator />)
+                        case "DMUserContextMenu": {
+                            patchDMUserContextMenu(this, props, res)
+                            break;
+                        }
+
+                        default: {
+                            console.error("ummm this context menu doesn't exist?...")
+                        }
+                    }
+
+                    return res;
+                }
+                firstRes.key = "TimezonesPatchedContextMenu"
+                firstRes.type.displayName = ogFunc.displayName
+            } catch (err) {
+                console.error("Could not patch the context menu", err)
+            }
+
+            return firstRes
+        }))(args[1]);
     })
+}
 
-    Patcher.after(WebpackModules.find(m => m.default?.displayName === 'DMUserContextMenu'), "default", (_this, [props], res) => {
-        const user: UserObject = props.user
+function patchDMUserContextMenu(_this, props, res) {
+    const user: UserObject = props.user
 
-        if (user.bot || Users.getCurrentUser().id === user.id) return
+    if (user.bot || Users.getCurrentUser().id === user.id) return
 
-        let userActions = res.props?.children?.props?.children?.[5]
-        if (!userActions) return
+    let menuGroup = res.props?.children?.props?.children?.[4]
+    if (!menuGroup) return
 
-        const state = useAsync(async () => ({
-            offlineTz: await getTimezone(user.id, { includeTT: false }),
-            timezone: await getTimezone(user.id, { includeTT: settings.get("tt", true) })
-        }))
+    const state = useAsync(async () => ({
+        offlineTz: await getTimezone(user.id, { includeTT: false }),
+        timezone: await getTimezone(user.id, { includeTT: settings.get("tt", true) })
+    }))
 
-        userActions.props.children.unshift(<MenuGroup label={state.loading ? <div style={{ opacity: .3 }}>Loading...</div> : null}>
-            {!state.loading ? <>
-                {!state.value?.timezone ? <MenuItem
-                    id="timezones-add-user"
-                    label="Add timezone locally"
-                    action={async () => { await ThisPlugin.prototype.openSettingsModal(user.id) }}
-                /> : <MenuItem
-                    id="timezones-actions"
-                    label="Timezone actions"
-                >
-                    {TimestampActions({
-                        id: user.id,
-                        timezone: state.value?.timezone,
-                        onEditTimezone: async id => await ThisPlugin.prototype.openSettingsModal(id, Boolean(!state.value?.offlineTz && state.value?.timezone)),
-                        onDeleteTimezone: async id => {
-                            ThisPlugin.prototype.onDeleteTimezone(id)
-                        },
-                        isOnline: !state.value?.offlineTz
-                    })}
-                </MenuItem>}
-            </> : <>
-                <MenuItem
-                    id="timezones-loading-add"
-                    label="Add timezone locally"
-                    disabled={true}
-                />
-                <MenuItem
-                    id="timezones-loading-actions"
-                    label="Timezone actions"
-                    disabled={true}
-                />
-            </>}
-        </MenuGroup>, <MenuSeparator />)
-    })
+    menuGroup.props.children.unshift(<MenuGroup label={state.loading ? <div style={{ opacity: .3 }}>Loading...</div> : null}>
+        {!state.loading ? <>
+            {!state.value?.timezone ? <MenuItem
+                id="timezones-add-user"
+                label="Add timezone locally"
+                action={async () => { await ThisPlugin.prototype.openSettingsModal(user.id) }}
+            /> : <MenuItem
+                id="timezones-actions"
+                label="Timezone actions"
+            >
+                {TimestampActions({
+                    id: user.id,
+                    timezone: state.value?.timezone,
+                    onEditTimezone: async id => await ThisPlugin.prototype.openSettingsModal(id, Boolean(!state.value?.offlineTz && state.value?.timezone)),
+                    onDeleteTimezone: async id => {
+                        ThisPlugin.prototype.onDeleteTimezone(id)
+                    },
+                    isOnline: !state.value?.offlineTz
+                })}
+            </MenuItem>}
+        </> : <>
+            <MenuItem
+                id="timezones-loading-add"
+                label="Add timezone locally"
+                disabled={true}
+            />
+            <MenuItem
+                id="timezones-loading-actions"
+                label="Timezone actions"
+                disabled={true}
+            />
+        </>}
+    </MenuGroup>, <MenuSeparator />)
+}
+
+function patchGuildChannelUserContextMenu(_this, props, res) {
+    const user: UserObject = props.user
+
+    if (user.bot || Users.getCurrentUser().id === user.id) return
+
+    let userActions = res.props?.children?.props?.children?.[5]
+    if (!userActions) return
+
+    const state = useAsync(async () => ({
+        offlineTz: await getTimezone(user.id, { includeTT: false }),
+        timezone: await getTimezone(user.id, { includeTT: settings.get("tt", true) })
+    }))
+
+    userActions.props.children.unshift(<MenuGroup label={state.loading ? <div style={{ opacity: .3 }}>Loading...</div> : null}>
+        {!state.loading ? <>
+            {!state.value?.timezone ? <MenuItem
+                id="timezones-add-user"
+                label="Add timezone locally"
+                action={async () => { await ThisPlugin.prototype.openSettingsModal(user.id) }}
+            /> : <MenuItem
+                id="timezones-actions"
+                label="Timezone actions"
+            >
+                {TimestampActions({
+                    id: user.id,
+                    timezone: state.value?.timezone,
+                    onEditTimezone: async id => await ThisPlugin.prototype.openSettingsModal(id, Boolean(!state.value?.offlineTz && state.value?.timezone)),
+                    onDeleteTimezone: async id => {
+                        ThisPlugin.prototype.onDeleteTimezone(id)
+                    },
+                    isOnline: !state.value?.offlineTz
+                })}
+            </MenuItem>}
+        </> : <>
+            <MenuItem
+                id="timezones-loading-add"
+                label="Add timezone locally"
+                disabled={true}
+            />
+            <MenuItem
+                id="timezones-loading-actions"
+                label="Timezone actions"
+                disabled={true}
+            />
+        </>}
+    </MenuGroup>, <MenuSeparator />)
+}
+
+// function contextMenuPatch(){
+//     Patcher.before(WebpackModules.getByProps("openContextMenuLazy"), "openContextMenuLazy", (_this, args, topRes) => {
+//         console.log({_this, args, topRes})
+//         args[1] = (ogFunc => args => ogFunc(args).then(render => props => {
+//             const firstRes = render(props)
+
+//             try {
+//                 console.log({ props, res: firstRes })
+//                 const ogFunc = firstRes?.type
+
+//                 if (!ogFunc) return firstRes
+
+//                 switch (ogFunc?.displayName) {
+//                     case "GuildChannelUserContextMenu": {
+//                         firstRes.type = (props) => {
+//                             useForceUpdate(); // Allow force re-rendering the entire context menu.
+//                             const res = ogFunc.call(this, props)
+
+//                             console.log(this, props, res)
+
+//                             return res;
+//                         }
+//                         break;
+//                     }
+
+//                     default : {
+//                         console.log("bruh")
+//                     }
+//                 }
+//             } catch (err) {
+//                 console.error("Could not patch the context menu", err)
+//             }
+
+//             return firstRes
+//         }))(args[1]);
+//     })
+// }
+
+const updateRefs = new Set();
+function useForceUpdate() {
+    const [, forceUpdate] = React.useReducer(e => !e, true);
+
+    React.useEffect(() => {
+        const listener = () => forceUpdate();
+        updateRefs.add(listener);
+
+        return () => void updateRefs.delete(listener);
+    });
 }
